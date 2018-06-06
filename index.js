fc72057ec195e2154d7b1c4b146c19516ba09271
github username <github email address>
2018-06-05 22:13:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+    this.regular = startDate;
+  }
 
+yearsExperienceFromBeginningOf(year) {
+
+    return (year - this.regular.slice(this.regular.length - 4));
+  }
+
+}
+
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+  //   if (this.endingLocation["vertical"] && this.beginningLocation["vertical"]) {
+  //   return Number(this.endingLocation["vertical"]) - Number(this.beginningLocation["vertical"])
+  // } else if (this.endingLocation["horizontal"] && this.beginningLocation["horizontal"]) {
+  //   let array = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+  //     let endThing = array.indexOf(this.endingLocation["horizontal"]);
+  //     let beginningThing = array.indexOf(this.beginningLocation["horizontal"]);
+  //     return endThing - beginningThing;
+  // }
+  let array = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+  let h = 0;
+  let start = array.indexOf(this.beginningLocation["horizontal"])
+  let end = array.indexOf(this.endingLocation["horizontal"]);
+  if (start > end) {
+    h = start - end;
+  } else if (end > start) {
+    h = end - start;
+  }
+let v = 0
+  if (this.beginningLocation["vertical"] > this.endingLocation["vertical"]) {
+    v = this.beginningLocation["vertical"] - this.endingLocation["vertical"];
+  } else if (this.beginningLocation["vertical"] < this.endingLocation["vertical"]) {
+    v = this.endingLocation["vertical"] - this.beginningLocation["vertical"];
+  }
+  return h + v;
+
+  }
+
+
+  estimatedTime (peakTime) {
+    if (peakTime) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    }
+  }
+
+
+
+
+}
